7fe9f4e85fb615f495f67eb9df97c5c56bdaf962
Lucas-Pedreira <lucasrpedreira@gmail.com>
2022-08-30 15:37:09 -0300

    excompleto

---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+let primeiroNumero = Number (prompt ("Insira um numero"))
+let segundoNumero = Number (prompt ("Insira outro numero"))
+
+console.log (primeiroNumero > segundoNumero)
+console.log (primeiroNumero === segundoNumero)
+console.log (primeiroNumero % segundoNumero === 0) 
+console.log (segundoNumero % primeiroNumero ===0)   
