b850a5b8b7eb4ac9f717b25c12b20e1b453be54a
Lucas-Pedreira <lucasrpedreira@gmail.com>
2022-08-30 14:01:45 -0300

    ex1completo

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let numeroPar = Number (prompt ("Insira um numero par"))
+let resto = Number (numeroPar % 2)
+
+console.log (resto)
+
+// qualquer numero par inserido o resultado é 0. 
+
+//qualquer numero impar inserido o resultado é 1.
